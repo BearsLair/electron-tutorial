e3c7d0df1fddfacd9f1aa1f2712a4983f0202b02
Patrick Gross <bears_lair@protonmail.com>
2025-03-18 20:27:47 -0700

    practice with preload scripts

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("node:path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
   win.loadFile("index.html");
 };
 
 app.whenReady().then(() => {
+  ipcMain.handle("ping", () => "pong");
   createWindow();
+});
+
+// Closing browser window(s) on Mac
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// Closing browser window(s) on Windows/Linux
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
